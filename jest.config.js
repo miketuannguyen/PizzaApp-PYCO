f340e2897e7916c63a5ece47a8e184a2582c646d
Luu Tuan Nguyen <miketuannguyen@gmail.com>
2020-04-27 16:25:25 +0700

    add jest

---
+++ jest.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  browser: false,
+  verbose: true,
+  reporters: ['default', 'jest-html-reporters'],
+  collectCoverage: true,
+  collectCoverageFrom: ['test/**/*.js'],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 10
+    }
+  },
+  coverageDirectory: 'coverage',
+  transform: {
+    '^.+\\.[t|j]sx?$': 'babel-jest'
+  },
+  transformIgnorePatterns: ['<rootDir>/node_modules/'],
+  preset: '@shelf/jest-mongodb'
+}
